dbfd4289a17b1d5ab6d5df4d00bf80036db91202
Nico Plyley <nplyley@gmail.com>
2020-12-27 14:51:14 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+def index():
+    return {'key': 'value'}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
